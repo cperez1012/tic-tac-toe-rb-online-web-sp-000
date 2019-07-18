f0cb9082e25bdf21c06881d3ec4ce6d478304022
Christian Perez <perezchristian1012@gmail.com>
2019-07-18 18:11:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,13 +53,11 @@ def turn(board)
   if valid_move?(board, index)
     player_token = current_player(board)
     move(board, index, player_token)
-
     display_board(board)
   else
     puts "try again"
     turn(board)
   end
-  display_board
 end
 
 def full?(board)
