4448517355c08679fd2aeb276d2d6d0d5fb8f573
Christian Perez <perezchristian1012@gmail.com>
2019-07-18 18:04:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,11 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board, index)
-index.between?(0,8) && !position_taken?(board, index)
+if index.between?(0,8) && !position_taken?(board, index)
+  return true
+else
+  return false
+end
 end
 #binding.pry
 
