c66adb5a2f17face968ded98def0bf14e3274bd4
Christian Perez <perezchristian1012@gmail.com>
2019-07-18 18:34:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,14 +41,16 @@ def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board
-  else
-    turn(board)
-  end
+def turn_count(board)
+  counter = 0
+  board.each do |player|
+    if player == "X" || player =="O"
+  counter += 1
+end
+end
+counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
