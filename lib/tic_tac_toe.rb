b31a5a84106314092105fb5acc1063bbad1c6e28
Christian Perez <perezchristian1012@gmail.com>
2019-07-18 18:21:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,8 +33,8 @@ def move(board, index, current_player)
   board[index] = current_player
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
 end
 
 def valid_move?(board, index)
