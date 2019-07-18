bb991979516740e0395425a9d020e3546e85b65a
Christian Perez <perezchristian1012@gmail.com>
2019-07-18 18:54:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, value = "X")
+    move(board, index, current_player(board))
   else
     puts "try again"
     turn(board)
